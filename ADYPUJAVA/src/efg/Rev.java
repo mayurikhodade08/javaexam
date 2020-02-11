89a658c506942cb1e077e7ff92b883226f61af01
ABHISHEK <ABHISHEK@DESKTOP-I1JFAF4>
2020-02-11 10:28:51 +0530

    reverse string done

---
+++ ADYPUJAVA/src/efg/Rev.java
@@ -0,0 +1,35 @@
+package efg;
+
+public class Rev {
+
+	public void reverseWordInMyString(String str)
+	   {
+		
+		String[] words = str.split(" ");
+		String reversedString = "";
+		for (int i = 0; i < words.length; i++)
+	        {
+	           String word = words[i]; 
+	           String reverseWord = "";
+	           for (int j = word.length()-1; j >= 0; j--) 
+		   {
+			
+			reverseWord = reverseWord + word.charAt(j);
+		   }
+		   reversedString = reversedString + reverseWord + " ";
+		}
+		System.out.println(str);
+		System.out.println(reversedString);
+	   }
+	   public static void main(String[] args) 
+	   {
+		Rev obj = new Rev();
+		obj.reverseWordInMyString("Hello Mayuri");
+		obj.reverseWordInMyString("practice session");
+	   }
+
+
+
+
+
+}
